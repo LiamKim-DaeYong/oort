c263aa25e73c9d5cfebe0ad6f3a608dcb9374058
DaeYong Kim <liamkim1018@gmail.com>
2026-08-16 16:28:07 +0900

    SonarCloud 연동 및 PR 품질 피드백 구성 (#30)
    * ci(quality): SonarCloud PR 분석 구성 (#21)
    
    * docs(quality): SonarCloud 초기 분석 기록 (#21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
 plugins {
+    alias(libs.plugins.sonarqube)
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.spring) apply false
     alias(libs.plugins.kotlin.jpa) apply false
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.spring.dependency.management) apply false
     alias(libs.plugins.ktlint) apply false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "LiamKim-DaeYong_oort")
+        property("sonar.organization", "liamkim-daeyong")
+    }
 }
 
 subprojects {
